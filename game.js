c23d6968e57c9a38ff1f669b121969fef77e28cc
Leschenko1001 <158287421+Leschenko1001@users.noreply.github.com>
2024-02-05 13:01:02 +0200

    додали платформу

---
+++ game.js
@@ -0,0 +1,45 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    
+        this.load.image('sky', 'assets/sky.png');
+        this.load.image('ground', 'assets/platform.png');
+        this.load.image('star', 'assets/star.png');
+        this.load.image('bomb', 'assets/bomb.png');
+        this.load.spritesheet('чувак',
+            'assets/dude.png',
+            {frameWidth: 32, frameHeight: 48}
+        );
+    
+}
+
+function create ()
+{
+    this.add.image(400, 300, 'sky');
+
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'platform').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'platform');
+    platforms.create(50, 250, 'platform');
+    platforms.create(750, 220, 'platform');
+}
+
+
+
+function update ()
+{
+}
